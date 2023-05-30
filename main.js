2dc7e17d6573fa1d488600a1ad971c90d04e06e9
jamesdurai <jamesdurai@outlook.com>
2023-05-30 10:21:48 +0530

    check loop prime

---
+++ main.js
@@ -0,0 +1,12 @@
+let a=17;
+for(i=2;i<=a;i++){
+	if(a%i==0){
+		if(i==a){
+			document.write('The given number is '+a+' and it is a prime number.');
+		}
+		else if(i!=a){
+			document.write('The given number is '+a+' and it is not a prime number.');
+			break;
+		}
+	}
+}
